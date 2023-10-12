e2c2f11746a53ee7507b86f5f06f20e0e0d5d5e8
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-10-12 19:59:42 +0530

    added clerk auth

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+export default {
+    providers: [
+      {
+        domain: "https://well-zebra-47.clerk.accounts.dev",
+        applicationID: "ably_spaces_fullstack",
+      },
+    ],
+  };
+  //applicationID is the name of the JWT template which we created
+  
