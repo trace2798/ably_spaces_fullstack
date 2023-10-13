b47ba6b40b76bbf3d7641d40136c03f6596428d3
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-10-13 15:54:06 +0530

    removed example width and spacing

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -34,28 +34,8 @@ export const getSpaceNameFromUrl = () => {
 
 export const REMOVE_USER_AFTER_MILLIS = 120_000;
 export const MAX_USERS_BEFORE_LIST = 10;
-export const HORIZONTAL_SPACING_OFFSET = 40;
-export const OVERLAP_AMOUNT = 40;
-export const AVATAR_WIDTH = 48;
 
-export function calculateRightOffset({
-  usersCount,
-  index = 0,
-}: {
-  usersCount: number;
-  index: number;
-}): number {
-  return usersCount > MAX_USERS_BEFORE_LIST
-    ? (index + 1) * HORIZONTAL_SPACING_OFFSET
-    : index * HORIZONTAL_SPACING_OFFSET;
-}
+
 export type Member = Omit<SpaceMember, "profileData"> & {
   profileData: { memberColor: string; name: string; imageUrl?: string };
 };
-
-export function calculateTotalWidth({ users }: { users: Member[] }): number {
-  return (
-    AVATAR_WIDTH +
-    OVERLAP_AMOUNT * Math.min(users.length, MAX_USERS_BEFORE_LIST + 1)
-  );
-}
